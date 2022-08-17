cf0bb293a62c385807e73732e6b970643710b9ac
Norbert305 <sealsnorbert@yahoo.com>
2022-08-17 18:45:46 -0400

    new added

---
+++ files/main2.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main () {
+  // Define magicNum and powerLevel below:
+  var magicNum, powerLevel int32 
+  
+  magicNum = 2048
+  powerLevel = 9001
+
+
+
+
+  fmt.Println("magicNum is:", magicNum, "powerLevel is:", powerLevel)
+  
+  
+  // Define amount and unit below:
+  amount, unit := 10, "doll hairs"
+  
+  fmt.Println(amount, unit, ", that's expensive...")
+
+  var num1, num2 int
+
+  num1 = 10
+  num2 = 20
+  fmt.Println("I like to have", num1, "apples and", num2, "oranges please")
+
+total, x := 10, "dollar"
+
+fmt.Println("Give me the total", x, "amount of", total, "snacks...")
+
+
+
+
+}
